c7149383a0d47aee220e6652c6b6feebc7094b87
Igor Zhuravlev <sijey06@mail.ru>
2025-07-03 03:18:41 +0500

    Correct final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.102.73', '127.0.0.1', 'localhost', 'inspar.ru']
+ALLOWED_HOSTS = ['51.250.102.73', '127.0.0.1', 'localhost', 'krakozabra.sytes.net']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
